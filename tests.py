bc853bd39362481e8c40977821a94a1babb07276
michaelloughnane <67080819+michaelloughnane@users.noreply.github.com>
2020-08-21 18:43:22 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -4,5 +4,5 @@ from src.fibonacci import recur_fibo
 def test_smallFib():
   assert recur_fibo(1) == 1, "Should be 1"
   
-def test_bigFib():
-  assert recur_fibo(100) == 354224848179261915075, "Should be 354224848179261915075"
+def test_biggerFib():
+  assert recur_fibo(10) == 55, "Should be 55"
