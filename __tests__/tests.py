3bd610deccec44ad56366220c1191cac6e65eb37
michaelloughnane <67080819+michaelloughnane@users.noreply.github.com>
2020-08-21 18:07:57 -0400

    Update tests.py

--- __tests__/tests.py
+++ __tests__/tests.py
@@ -1,5 +1,7 @@
-import sample-setup
-from src.fibonacci import recur_fibo
+import sys
+sys.path.append('michaelloughnane/sample-setup/src')
+import fibonacci
+from fibonacci import recur_fibo
 
 def test_smallFib():
   assert recur_fibo(1) == 1, "Should be 1"
